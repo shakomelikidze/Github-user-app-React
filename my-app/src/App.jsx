cf832410722b823d55a90672ad7a5a68ec5b7f03
shakomelikidze <shako456@gmail.com>
2023-11-12 13:18:44 +0400

    .

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState();
+  const darkToggle = () => {
+    setDarkTheme(!darkTheme);
+  }
   return (
     <>
-      
+      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde aperiam dolor, laboriosam odit commodi obcaecati nulla doloremque maiores eligendi, iusto non dolorum nesciunt perferendis eius nisi neque exercitationem rerum fuga?</div>
+      <button onClick={darkToggle}>Set dark</button>
     </>
   )
 }
 
-export default App
+export default App;
